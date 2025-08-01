be7f0705ce915234002da715f1837ded0abc87c3
Danh Doan <nkocdanh58@gmail.com>
2025-08-01 16:14:56 +0700

    Refresh CartEntity

--- Mango.Services.ShoppingCartAPI/Migrations/20250801090807_OrderStatusUpdate1.Designer.cs
+++ Mango.Services.ShoppingCartAPI/Migrations/20250801090807_OrderStatusUpdate1.Designer.cs
@@ -4,6 +4,7 @@ using Mango.Services.ShoppingCartAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Mango.Services.ShoppingCartAPI.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250801090807_OrderStatusUpdate1")]
+    partial class OrderStatusUpdate1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
